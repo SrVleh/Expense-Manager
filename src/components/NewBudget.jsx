6725d4e5366e02b8044c3aa74a1ddc8d3c20d82e
TrueVale <srvleh@gmail.com>
2022-02-15 19:11:43 +0100

    Components and initial configurations

---
+++ src/components/NewBudget.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NewBudget = () => {
+  return (
+    <div className='budget-container container shadow'>
+        <form className='form'>
+            <div className='field'>
+                <label>Set Budget</label>
+                <input 
+                    className='new-budget'
+                    type="text" 
+                    placeholder='Add your budget'
+                />
+            </div>
+
+
+            <input type="submit" value="Add"/>
+        </form>
+    </div>
+  );
+}
+
+export default NewBudget;
