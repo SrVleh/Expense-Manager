21d0f7a5c3f723c166e914eaf1c0fffdc29b258f
TrueVale <srvleh@gmail.com>
2022-02-17 19:09:32 +0100

    Creating expenses states

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,11 @@
-
+import { useState } from 'react'
 import CloseModal from '../img/close.svg'
 
 const Modal = ({ setModal, animModal, setAnimModal }) => {
+
+    const [name, setName] = useState('')
+    const [quantity, setQuantity] = useState ('')
+    const [category, setCategory] = useState('')
 
     const closeModal = () => {
         setAnimModal(false)
@@ -27,24 +31,30 @@ const Modal = ({ setModal, animModal, setAnimModal }) => {
                 <input 
                     id='name'
                     type='text'
-                    placeholder='Add the expense name' />
+                    placeholder='Add the expense name'
+                    value={ name } 
+                    onChange={ e => setName(e.target.value) } />
             </div>
             <div className='field'>
                 <label htmlFor="quantity">Quantity</label>
                 <input 
                     id='quantity'
                     type='number'
-                    placeholder='Add expense quantity: ex. 400' />
+                    placeholder='Add expense quantity: ex. 400' 
+                    value={ quantity }
+                    onChange={ e => setQuantity(Number(e.target.value)) }/>
             </div>
             <div className='field'>
                 <label htmlFor="category">Category</label>
                 <select 
-                    id="category" >
+                    id="category"
+                    value={ category }
+                    onChange={ e => setCategory(Number(e.target.value)) }>
                         <option value="">-- Select --</option>
                         <option value="savings">Savings</option>
                         <option value="food">Food</option>
                         <option value="house">House</option>
-                        <option value="leisure">Leisur</option>
+                        <option value="leisure">Leisure</option>
                         <option value="health">Health</option>
                         <option value="subscriptions">Subscriptions</option>
                         <option value="other">Other</option>
