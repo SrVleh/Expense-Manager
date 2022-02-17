970bc75cbe2ce50730fffd327dfa4cceae031c90
TrueVale <srvleh@gmail.com>
2022-02-17 18:55:25 +0100

    Modal form created

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ setModal, animModal, setAnimModal }) => {
 
     const closeModal = () => {
         setAnimModal(false)
-        
+
         setTimeout(() => {
             setModal(false)
         }, 500)
@@ -22,6 +22,36 @@ const Modal = ({ setModal, animModal, setAnimModal }) => {
 
         <form className={`form ${ animModal ? 'anim' : 'close' }`}>
             <legend>New Expense</legend>
+            <div className='field'>
+                <label htmlFor="name">Expense Name</label>
+                <input 
+                    id='name'
+                    type='text'
+                    placeholder='Add the expense name' />
+            </div>
+            <div className='field'>
+                <label htmlFor="quantity">Quantity</label>
+                <input 
+                    id='quantity'
+                    type='number'
+                    placeholder='Add expense quantity: ex. 400' />
+            </div>
+            <div className='field'>
+                <label htmlFor="category">Category</label>
+                <select 
+                    id="category" >
+                        <option value="">-- Select --</option>
+                        <option value="savings">Savings</option>
+                        <option value="food">Food</option>
+                        <option value="house">House</option>
+                        <option value="leisure">Leisur</option>
+                        <option value="health">Health</option>
+                        <option value="subscriptions">Subscriptions</option>
+                        <option value="other">Other</option>
+                </select>
+            </div>
+
+            <input type="submit" value='Add expense' />
         </form>
     </div>
   );
