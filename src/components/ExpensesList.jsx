6cf7d4ff8c3f83301e2292f83c8c7bb3a6aea9a6
TrueVale <srvleh@gmail.com>
2022-02-22 22:49:52 +0100

    Showing filtered expenses

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -1,18 +1,37 @@
 
+import { string } from "prop-types";
 import Expense from "./Expense";
 
-const ExpensesList = ({ expenses, setExpenseEdit, deleteExpense }) => {
+const ExpensesList = ({ expenses, setExpenseEdit, deleteExpense, filter, filteredExpenses }) => {
   return (
     <div className="expenses-list container">
-        <h2>{ expenses.length ? 'Expenses' : 'There are no expenses yet'}</h2>
-        {expenses.map( expense => (
-            <Expense
-                key = { expense.id }
-                expense = { expense }
-                setExpenseEdit = { setExpenseEdit }
-                deleteExpense = { deleteExpense } 
-            />
-        ))}
+        {
+          filter ? (
+            <>
+              <h2>{ filteredExpenses.length ? `${ filter.charAt(0).toUpperCase() + filter.slice(1) } expenses` : `There are no expenses on ${ filter }`}</h2>
+              {filteredExpenses.map( expense => (
+                <Expense
+                    key = { expense.id }
+                    expense = { expense }
+                    setExpenseEdit = { setExpenseEdit }
+                    deleteExpense = { deleteExpense } 
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              <h2>{ expenses.length ? 'Expenses' : 'There are no expenses yet'}</h2>
+              {expenses.map( expense => (
+                <Expense
+                    key = { expense.id }
+                    expense = { expense }
+                    setExpenseEdit = { setExpenseEdit }
+                    deleteExpense = { deleteExpense } 
+                />
+              ))}
+            </>
+          )
+        }
     </div>
   );
 }
